ab48779dac8cc1ca3aebfda801cfe15b6f46ca5f
Angldrie101 <89821301+Angldrie101@users.noreply.github.com>
2024-06-14 17:59:20 +0800

    Add project files.

---
+++ UCGrab/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UCGrab.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(String ReturnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Error = String.Empty;
+            ViewBag.ReturnUrl = ReturnUrl;
+
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+          
+            return View();
+        }
+
+    }
+}
